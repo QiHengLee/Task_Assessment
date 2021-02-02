ad183fb3f607f11ef19fa3e45aee9e8f11a69094
QiHengLee <57242546+QiHengLee@users.noreply.github.com>
2021-02-02 12:57:31 -0600

    edit index.js file

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 // ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password'
 
-const mysql = require('mysql');
+// const mysql = require('mysql');
 var express = require("express");
 var app = express();
 
 
-var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'password',
-    database : 'testing'
-});
+// var connection = mysql.createConnection({
+//     host     : 'localhost',
+//     user     : 'root',
+//     password : 'password',
+//     database : 'testing'
+// });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-});
+// connection.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+// });
   
 app.listen(9007, () => console.log('Listening on port 9007!'));
 
