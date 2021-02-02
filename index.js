813ddca58a5fcb8c97981a58a9fdfd29fc334e36
QiHengLee <57242546+QiHengLee@users.noreply.github.com>
2021-02-02 12:50:58 -0600

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password'
+
+const mysql = require('mysql');
+var express = require("express");
+var app = express();
+
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'password',
+    database : 'testing'
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+  
+app.listen(9007, () => console.log('Listening on port 9007!'));
+
+app.get('/',function(req, res) {
+  res.sendFile(__dirname + '/main.html');
+});
+
+app.use('/', express.static(__dirname + '/'));
